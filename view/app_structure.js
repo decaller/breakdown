a68b45957be8bd4824fcfe09e0c3c950f1f16843
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2015-12-07 18:34:37 +0700

    fixing accordion tree

--- view/app_structure.js
+++ view/app_structure.js
@@ -8,19 +8,25 @@ var app_structure =
   cols : [
     {body:view_main_breakdown__project_breakdown},
     {view:"resizer", id:"breakdown_resizer",borderless:true},
-    {header: "",id:"search_pane" ,borderless:true,headerAltHeight:0,collapsed: true, css:"pane", body:
-        {
-        view : "multiview",
-        borderless:true,
-        keepViews : true,
-        cells : [
-          view_add_breakdown__search,
-          view_add_mtw__search
-        ]
-        }
-      },
-      {view:"resizer",borderless:true, id:"details-search_resizer"},
-      {header: "",id:"details_pane", css:"pane",borderless:true,headerAltHeight:0,collapsed: true, body: view_main_breakdown__breakdown_details}
+    {
+      header:"",
+      body:{
+      cols:[
+        {header: "",id:"search_pane" ,borderless:true,headerAltHeight:0,collapsed: true, css:"pane", body:
+          {
+          view : "multiview",
+          borderless:true,
+          keepViews : true,
+          cells : [
+            view_add_breakdown__search,
+            view_add_mtw__search
+          ]
+          }
+        },
+        {view:"resizer",borderless:true, id:"details-search_resizer"},
+        {header: "",id:"details_pane", css:"pane",borderless:true,headerAltHeight:0,collapsed: true, body: view_main_breakdown__breakdown_details}  
+    ]}
+    }
     ]
           
 }
