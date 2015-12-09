4d6038e0be9b41fe959fd4ca8b0fc799060049a7
Harridi Ilman Tovid <harridiilmantovid@gmail.com>
2015-12-10 00:59:23 +0700

    project list done

--- cons/webix_logic.js
+++ cons/webix_logic.js
@@ -247,3 +247,157 @@ function childTotal_search(item) {
   	return total;
 };
 
+//BACKEND CONNECTION PART - BE CAREFUL
+//reserve variable
+var account = {};
+var activeProject = {};
+
+        
+function connect(){
+      //take all values
+      var value = $$("connectForm").getValues();
+    	account ={
+            url : value.url, 
+            db : value.db, 
+            user : value.user, 
+            pass : value.pass 
+          };
+          
+      //clear All database    
+  		$$("project_list_dataview").clearAll();
+      $$("treetable_main_breakdown").clearAll();
+      
+      
+      
+      //make connection and load project_list_dataview
+  		var url = "odata->" + account.url + "/api1/" + account.db + "/collections/project_list";
+  		$$("project_list_dataview").load(url);
+  		var dp = new webix.DataProcessor({  
+          master:$$("project_list_dataview"), 
+          url:url
+        });
+      $$("project_list").show();
+      
+};
+
+//OData connection lib - DO NOT TOUCH
+webix.proxy.odata = {
+	$proxy:true,
+	load:function(view, callback, details){
+		
+		var url = this._odataurl(view, details);
+		this._load(view, callback, url);
+	},
+	_odataurl:function(view, details){
+		var url = this.source;
+		
+		url = url.replace(/(\$format=(.*)&)|(\$format=(.*)$)/, "").replace(/\?$/, "");
+		url += (url.indexOf("?") == -1 ) ? "?": "&";
+		url += "$format=json";
+		
+		if(this.dynamicLoading){
+			url += "&$inlinecount=allpages";
+			if(!details) url+="&$top="+view.config.datafetch;
+		}
+
+		if(details){
+			var start = details.from, count = details.count;
+			
+			//page && scroll
+			if(start && count!==-1){
+				if(start<0) start = 0;
+				url += "&$skip="+start+"&$top="+count;
+			}
+
+			//filter && sort
+			if(details.sort)
+				url += "&$orderby="+details.sort.id+" "+details.sort.dir;
+
+			if(details.filter){
+				var filters = [];
+				for (var key in details.filter){
+					if(details.filter[key])
+						filters.push("startswith("+key+",'"+(details.filter[key]?details.filter[key]:"")+"')");
+				}
+				if(filters.length) url +="&$filter="+filters.join(" and ");
+			}
+		}
+		return url;
+	},
+	_load:function(view, callback, url){
+			webix.ajax().headers({
+				"Authorization": "Basic " + btoa(account.user + ":" + account.pass)
+			}).get(url).then(function(data){
+				data = data.json();
+				if(data.d){
+					if(data.d.__count){
+						var records = {
+							data:data.d.results,
+							pos:data.d.__skip||0,
+							total_count:data.d.__count*1
+						};
+					}
+					else
+						var records = data.d.results;
+					webix.ajax.$callback(view, callback, "", records, -1);
+				}
+			});
+	},
+	save:function(view, update, dp, callback){
+		var url = this.source,
+			mode = update.operation,
+			data = update.data,
+			editLink = url.replace(/\/$/, "")+"("+data["id"]+")";
+
+		if(mode == "insert") delete data.id;
+		data = JSON.stringify(data);
+
+		//call odata URI
+		if(mode == "insert"){
+			webix.ajax().headers({
+				"Authorization": "Basic " + btoa(account.user + ":" + account.pass),
+				"Content-type":"application/json"
+			}).post(url, data, callback);
+		} else if (editLink){
+			if (mode == "update"){
+				webix.ajax().headers({
+					"Authorization": "Basic " + btoa(account.user + ":" + account.pass),
+					"Content-type":"application/json"
+				}).put(editLink, data, callback);
+			} else if (mode == "delete") {
+				webix.ajax().headers({
+					"Authorization": "Basic " + btoa(account.user + ":" + account.pass),
+					"Content-type":"application/json"
+				}).del(editLink, data, callback);
+			}
+		}
+	},
+	result:function(state, view, dp, text, data, loader){
+		data = data.json();
+		var obj = state;
+		if (data){
+			obj = data.d.results || {status:"error"};
+			obj.newid = data.d.results.id;
+
+			dp.processResult(state, obj);
+		}
+	}
+};
+
+webix.proxy.odataDynamic = {
+	init:function(){
+		webix.extend(this, webix.proxy.odata);
+	},
+	dynamicLoading:true
+};
+//OData lib done
+
+//make new project function
+function newProject(){
+    var value = $$("makeNewProjectForm").getValues();
+    $$("project_list_dataview").add({
+      name : value.name,
+      id : value.id,
+      date : new Date()
+    })
+}
--- view/app_login.js
+++ view/app_login.js
@@ -21,7 +21,7 @@ var view_login_breakdown = {
 							{ view : "text", label : "Database", name : "db", value: "b099" },
 							{ view : "text", label : "User", name : "user", value:"t099" },
 							{ view : "text", label : "Password", name : "pass", value:"4614" },
-							{ view : "button", label : "connect" }
+							{ view : "button", label : "Connect", click:connect}
 						]
 					},
 					{
@@ -35,8 +35,7 @@ var view_login_breakdown = {
 						rows : [
 							
 							{ view : "text", label : "Name", name : "name"},
-							{ view : "text", label : "Id", name : "id"},
-							{ view : "button", label : "Build" }
+							{ view : "button", label : "Build", click:newProject }
 						]
 					},
 					{
@@ -73,18 +72,14 @@ var view_login_breakdown = {
 							},
 							{
 								view:"dataview", 
-								id:"dataview1",
+								id:"project_list_dataview",
 								select:true,
 								type: {
 									height: 60
 								},
-								template:"<div class='webix_strong'>#title#</div> Year: #year#, rank: #rank#",
-								data:[
-									{ id:1, title:"The Shawshank Redemption", year:1994, votes:678790, rating:9.2, rank:1},
-									{ id:2, title:"The Godfather", year:1972, votes:511495, rating:9.2, rank:2},
-									{ id:3, title:"The Godfather: Part II", year:1974, votes:319352, rating:9.0, rank:3},
-									{ id:4, title:"The Good, the Bad and the Ugly", year:1966, votes:213030, rating:8.9, rank:4}
-								]
+								template:"<div class='webix_strong'>#name#</div> Date: #date#",
+								
+								
 							}
 						]
 				}
